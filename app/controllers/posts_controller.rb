46c509db2ac47058a71bd7f86ef2a26f02bbb67c
Toni-Ty <antonique2479@yahoo.com>
2020-01-24 02:15:22 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,30 +2,29 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
-  end
-
-  def edit
-  end
-
-  def update
-    new_post = Post.new(post_params)
-
-    if new_post.valid?
-      @post.update(post_params)
-      redirect_to post_path(@post)
-
-    else
-      render :edit
-  end
-end
-
-  private
-
-  def post_params
-    params.permit(:category, :content, :title)
-  end
-
-  def set_post!
-    @post = Post.find(params[:id])
-  end
-end
+   end
+ 
+   def edit
+   end
+ 
+   def update
+     new_post = Post.new(post_params)
+ 
+     if new_post.valid?
+       @post.update(post_params)
+       redirect_to post_path(@post)
+     else
+       render :edit
+     end
+   end
+ 
+   private
+ 
+   def post_params
+     params.permit(:category, :content, :title)
+   end
+ 
+   def set_post!
+     @post = Post.find(params[:id])
+   end
+ end
