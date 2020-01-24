386d4b372910579481237472e8ea6e93798d6aa3
Toni-Ty <antonique2479@yahoo.com>
2020-01-24 02:19:51 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
   validates :content, length: {minimun: 100}
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 end
